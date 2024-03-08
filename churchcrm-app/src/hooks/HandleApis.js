8b1381bffd7789bde43fce9cb9d0ce238a4ed717
Njeri <85888329+Njeri-Ngugi@users.noreply.github.com>
2024-03-08 13:48:21 +0300

    Correct URL link

--- churchcrm-app/src/hooks/HandleApis.js
+++ churchcrm-app/src/hooks/HandleApis.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://2620-197-237-60-108.ngrok-free.app';
+export const BASE_URL = 'https://karencommunitychurch.org:4433';
 const config = {
   Accept: 'application/json',
   'Content-Type': 'application/json',
