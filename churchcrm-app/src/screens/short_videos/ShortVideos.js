701bf34d033a321369b421c1a5460d61f8571e62
Njeri <85888329+Njeri-Ngugi@users.noreply.github.com>
2024-03-11 08:54:39 +0300

    Debug promises, authentication

--- churchcrm-app/src/screens/short_videos/ShortVideos.js
+++ churchcrm-app/src/screens/short_videos/ShortVideos.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   ScrollView,
@@ -6,32 +6,27 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from 'react-native';
-import {styles} from './ShortVideosCss';
-import {fetchDataByEndpoint} from '../../hooks/HandleApis';
-import {BASE_URL} from '../../hooks/HandleApis';
+import { styles } from './ShortVideosCss';
+import { fetchAllData } from '../../hooks/HandleApis';
+import { BASE_URL } from '../../hooks/HandleApis';
 import Video from 'react-native-video';
-
-export const fetchShortVideo = async () => {
-  return fetchDataByEndpoint('fetchShortVideo');
-};
 
 export default function ShortVideos() {
   const [shortvideoData, setShortvideoData] = useState([]);
   const [shortvideoLoading, setShortvideoLoading] = useState(true);
-  const [selectedVideo, setSelectedVideo] = useState({
-    id: 0,
-    date: 'Default Date',
-    title: 'Default Title(default video)',
-    thumbnail_path: `${BASE_URL}/ShortVideoThumbnails/default_thumbnail.jpeg`,
-    videourl: `${BASE_URL}/Shortvideos/1709727126.mp4`,
-  });
+  const [selectedVideo, setSelectedVideo] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const videos = await fetchShortVideo();
-        setShortvideoData(videos);
-        setShortvideoLoading(false);
+        const responses = await fetchAllData();
+        if (responses && Array.isArray(responses)) {
+          setShortvideoData(responses[4]);
+          setShortvideoLoading(false);
+          setSelectedVideo(responses[4][0])
+        } else {
+          console.error('Error fetching data: Responses array is null or not an array');
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -41,7 +36,7 @@ export default function ShortVideos() {
 
   const handleVideoClick = video => {
     const videourl = `${BASE_URL}/Shortvideos/${video.video_path}`;
-    setSelectedVideo({...video, videourl});
+    setSelectedVideo({ ...video, videourl });
     console.log(selectedVideo);
   };
 
@@ -54,7 +49,7 @@ export default function ShortVideos() {
               source={{
                 uri: `${BASE_URL}/Shortvideos/${selectedVideo.video_path}`,
               }}
-              style={{width: 300, height: 300}}
+              style={{ width: 300, height: 300 }}
               controls={true}
               ref={ref => {
                 this.player = ref;
@@ -67,28 +62,31 @@ export default function ShortVideos() {
           <View style={styles.videoRow}>
             {shortvideoLoading ? (
               <Text style={styles.loadingText}>Loading videos...</Text>
-            ) : shortvideoData.length > 0 ? (
+            ) : shortvideoData && shortvideoData.length > 0 ? (
               shortvideoData.map(video => (
-                <TouchableOpacity
-                  key={video.id}
-                  style={styles.videoColumn}
-                  onPress={() => handleVideoClick(video)}>
-                  <ImageBackground
-                    style={styles.videoImage}
-                    imageStyle={styles.videoImage}
-                    resizeMode="cover"
-                    source={{
-                      uri: `${BASE_URL}/ShortVideoThumbnails/${video.thumbnail_path}`,
-                    }}>
-                    <View style={styles.videoOverlay}>
-                      <Text>Video Overlay</Text>
-                    </View>
-                    <View style={styles.videoTextContainer}>
-                      <Text style={styles.videoDate}>{video.date}</Text>
-                      <Text style={styles.videoText}>{video.title}</Text>
-                    </View>
-                  </ImageBackground>
-                </TouchableOpacity>
+                video.id !== selectedVideo.id ? (
+                  <View key={video.id}>
+                    <TouchableOpacity
+                      style={styles.videoColumn}
+                      onPress={() => handleVideoClick(video)}>
+                      <ImageBackground
+                        style={styles.videoImage}
+                        imageStyle={styles.videoImage}
+                        resizeMode="cover"
+                        source={{
+                          uri: `${BASE_URL}/ShortVideoThumbnails/${video.thumbnail_path}`,
+                        }}>
+                        <View style={styles.videoOverlay}></View>
+                        <View style={styles.videoTextContainer}>
+                          <Text style={styles.videoDate}>{video.date}</Text>
+                          <Text style={styles.videoText}>{video.title}</Text>
+                        </View>
+                      </ImageBackground>
+                    </TouchableOpacity>
+                  </View>
+                ) : (
+                  <></>
+                )
               ))
             ) : (
               <Text style={styles.loadingText}>No videos available</Text>
@@ -99,3 +97,4 @@ export default function ShortVideos() {
     </View>
   );
 }
+
